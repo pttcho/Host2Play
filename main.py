9b5926292eea17722b6a03f2c02df1ced9869512
pttcho <131829070+pttcho@users.noreply.github.com>
2026-08-06 15:18:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=65bd476a-deb6-4585-87fb-7adbe34809e8",
+    "https://host2play.gratis/server/renew?i=1b8280df-1118-4601-9e5f-41e6c7ae6855",
     # 添加更多链接
 ]
 
